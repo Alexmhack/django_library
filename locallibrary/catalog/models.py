acbabdacf10dc8387371b8e8c4b1870a03d52954
alexHooper <alexmohack@gmail.com>
2018-07-19 08:45:18 +0530

    Author is not defined yet so 'Author' in quotes

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -12,7 +12,7 @@ class Genre(models.Model):
 
 class Book(models.Model):
 	title = models.CharField(max_length=200)
-	author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
+	author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
 	summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
 	isbn = models.CharField('ISBN', max_length=13, help_text='13 character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
 	genre = models.ManyToManyField(Genre, help_text='Select a Genre for this book')
