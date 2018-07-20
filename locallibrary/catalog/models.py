fade2463f3e0de02246812f8f7110fa1ba24254c
alexHooper <alexmohack@gmail.com>
2018-07-20 21:52:39 +0530

    add logic for borrowing and overdue of book

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -2,6 +2,9 @@ import uuid
 
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
+
+from datetime import date
 
 
 class Genre(models.Model):
@@ -42,6 +45,7 @@ class BookInstance(models.Model):
 	imprint = models.CharField(max_length=200)
 	due_back = models.DateField(null=True, blank=True)
 	language = models.ForeignKey(Language, on_delete=models.SET_NULL, null=True)
+	borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
 	LOAN_STATUS = (
 		('m', 'Maintenance'),
@@ -57,6 +61,12 @@ class BookInstance(models.Model):
 
 	def __str__(self):
 		return '{0} ({1})'.format(self.id, self.book.title)
+
+	@property
+	def is_overdue(self):
+		if self.due_back and date.today() > self.due_back:
+			return True
+		return False
 
 
 class Author(models.Model):
