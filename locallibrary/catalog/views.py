d663ebf12ca3d36d47aad77d041f72325cc42661
alexHooper <alexmohack@gmail.com>
2018-07-20 22:28:23 +0530

    add list view and url for loaned book by users

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Book, BookInstance, Genre, Author
 
@@ -60,3 +61,13 @@ class AuthorListView(generic.ListView):
 
 class AuthorDetailView(generic.DetailView):
 	model = Author
+
+
+class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
+	model = BookInstance
+	template_name = 'catalog/bookinstance_list_borrowed_user.html'
+	paginate_by = 10
+
+	def get_queryset(self):
+		return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
+		
