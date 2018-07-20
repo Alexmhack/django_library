fde93fd152dd3693a2e7cabf81c66e6e899aa897
alexHooper <alexmohack@gmail.com>
2018-07-20 21:34:32 +0530

    add functionality to send password_reset email

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
+from django.urls import reverse_lazy
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -122,3 +124,14 @@ STATIC_URL = '/static/'
 
 # login redirects to the home page (Defualts to accounts/profile)
 LOGIN_REDIRECT_URL = '/'
+LOGIN_URL = reverse_lazy('login')
+LOGOUT_URL = reverse_lazy('logout')
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = '78030psg@gmail.com'
+EMAIL_HOST_PASSWORD = 'thisisauselessemail'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+# django smtp email backend
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
