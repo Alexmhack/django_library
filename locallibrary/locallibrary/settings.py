3c994659ae56f67a122fba74a6a2ee4c9391cb6f
alexHooper <alexmohack@gmail.com>
2018-07-20 20:51:55 +0530

    add templates for login and logout view

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -119,3 +119,6 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# login redirects to the home page (Defualts to accounts/profile)
+LOGIN_REDIRECT_URL = '/'
