eafa73dbe16ecafd3e217e200d2246a47c7a3132
alexHooper <alexmohack@gmail.com>
2018-07-21 10:59:38 +0530

    add all borrowed book view for librarian members

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import PermissionRequiredMixin
 
 from .models import Book, BookInstance, Genre, Author
 
@@ -71,3 +72,12 @@ class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
 	def get_queryset(self):
 		return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
 		
+
+class AllBorrowedBookListView(PermissionRequiredMixin, generic.ListView):
+	model = BookInstance
+	template_name = 'catalog/all_borrowed_book_list.html'
+	paginate_by = 10
+	permission_required = 'catalog.can_mark_returned'
+
+	def get_queryset(self):
+		return BookInstance.objects.filter(status__exact='o').order_by('due_back')
