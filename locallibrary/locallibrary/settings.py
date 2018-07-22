2be03380ab9f7f8ee5fa7e3c51c39b0dbeca9a9d
alexHooper <alexmohack@gmail.com>
2018-07-23 01:05:43 +0530

    add environ variables for deployment

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -22,10 +22,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '69g206^@mj(!k$_hf*+5_imvhfplrotx%l10)w5n!o1*8$d@e8'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '69g206^@mj(!k$_hf*+5_imvhfplrotx%l10)w5n!o1*8$d@e8')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
