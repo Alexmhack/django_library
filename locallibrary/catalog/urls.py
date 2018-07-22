8b811c281b0cd29288fd65c24b3aaeadfa5489c1
alexHooper <alexmohack@gmail.com>
2018-07-22 21:52:10 +0530

    add urls for book CRUD

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -30,3 +30,10 @@ urlpatterns += [
 	path('author/<int:pk>/update', views.AuthorUpdate.as_view(), name='author_update'),
 	path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author_delete'),
 ]
+
+# urls for books generic edit
+urlpatterns += [
+	path('book/create/', views.BookCreate.as_view(), name='book_create'),
+	path('book/<int:pk>/update/', views.BookUpdate.as_view(), name='book_update'),
+	path('book/<int:pk>/delete/', views.BookDelete.as_view(), name='book_delete'),
+]
