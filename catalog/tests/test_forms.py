8c2b6f10421cb75052f1c97f2db1dd46df815326
alexHooper <alexmohack@gmail.com>
2018-07-23 02:41:30 +0530

    change root directory of project

---
+++ catalog/tests/test_forms.py
@@ -0,0 +1,3 @@
+from django.test import TestCase
+
+from .forms import RenewBookModelForm
--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '69g206^@mj(!k$_hf*+5_imvhfplro
 # DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
