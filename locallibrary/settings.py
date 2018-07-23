9ea26a16dadfddbe72f3e9249afdc5415102bc4b
alexHooper <alexmohack@gmail.com>
2018-07-23 15:49:54 +0530

    reset to previous code

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '69g206^@mj(!k$_hf*+5_imvhfplro
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = False
+DEBUG = bool(os.environ.get('DEBUG_VALUE', False))
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
